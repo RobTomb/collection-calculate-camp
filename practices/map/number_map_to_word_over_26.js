59d044dda27ff58d31160944c72a9b53a473be79
robtomb <robtomb@outlook.com>
2017-08-01 17:16:46 +0800

    map was finished

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,6 +1,26 @@
 'use strict';
+// MAX number 26 * 27
+const alphabat = [ 'z','a','b','c','d','e',
+  'f','g','h','i','j','k',
+  'l','m','n','o','p','q',
+  'r','s','t','u','v','w','x','y','z' ]
+function getLetter(num){
+	let remainder = num % 26;
+	let multiples = num / 26;
+	let letter = '';
+	if(multiples > 1 && remainder !== 0 )
+		letter += alphabat[parseInt(multiples)];
+	if( multiples > 1 && remainder === 0)
+		letter += alphabat[parseInt(multiples)-1];
+	letter += alphabat[remainder];
+	return letter;
+}
+
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return collection.map( (item)=>{
+  	return getLetter(item);
+  });
 };
 
 module.exports = number_map_to_word_over_26;
+
--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,6 +1,9 @@
 'use strict';
 var rank_asc = function(collection){
-  return [6,5,4,3,2];
+
+  return collection.sort( (it,me)=>{
+  	return me - it;
+  });
 };
 
 module.exports = rank_asc;
