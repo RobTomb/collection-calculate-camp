353c34a440081db6c3d4408726678c11a3aea043
robtomb <robtomb@outlook.com>
2017-08-02 10:55:55 +0800

    fixed bug about can't npm test

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -1,23 +1,5 @@
 'use strict';
 
-function getLetterInterval(number_a,number_b){
-	let result = [];
-	let mom = parseInt(number_b / number_a);
-	while( mon ){
-		while(number_a <= number_b){
-			let letter = String.fromCharCode(number_a+77);
-			let site = result.indexOf(letter);
-			if(site === -1)
-				result.push(letter);
-			else
-				result[site] += letter;
-			
-		}
-		number_a 
-		mon -= 1;		
-	}
-	result result;
-}
 
 function get_letter_interval_2(number_a, number_b) {
 
